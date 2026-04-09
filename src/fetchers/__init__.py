5ac9ad374646273d73dc7187a279f5b28e124e8f
nninnsungkim <kim5333@purdue.edu>
2026-04-09 18:17:47 -0400

    Fix import: BlogScraper not CompanyScraper

--- src/fetchers/__init__.py
+++ src/fetchers/__init__.py
@@ -1,5 +1,5 @@
 from .google_news import fetch_all_news, fetch_neurotech_news, fetch_productivity_news
-from .company_scraper import scrape_company_updates, CompanyScraper
+from .company_scraper import scrape_company_updates, BlogScraper, LinkedInScraper
 from .rss_feeds import fetch_rss_news, RSSFetcher
 
 __all__ = [
@@ -7,7 +7,8 @@ __all__ = [
     'fetch_neurotech_news',
     'fetch_productivity_news',
     'scrape_company_updates',
-    'CompanyScraper',
+    'BlogScraper',
+    'LinkedInScraper',
     'fetch_rss_news',
     'RSSFetcher',
 ]
